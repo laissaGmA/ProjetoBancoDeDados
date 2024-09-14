2023bda0bfdcdf90e1d662c620368dce3bdabb93
Jeffersonalanmm <jefferson.alan.jm@gmail.com>
2024-09-14 18:16:24 -0300

    adicionar fornecedor

--- src/main/java/com/example/oracle_db_project/model/Supplier.java
+++ src/main/java/com/example/oracle_db_project/model/Supplier.java
@@ -1,9 +1,14 @@
 package com.example.oracle_db_project.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "SUPPLIER", schema = "SYSTEM")
+@Table(name = "SUPPLIER", schema = "system")
 public class Supplier {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "supplier_seq")
@@ -14,12 +19,13 @@ public class Supplier {
     private String supplier_country;
     private String supplier_street;
     private String supplier_complement;
-    private int supplier_number;
+    private long supplier_number;
     private int supplier_type;
     private String supplier_document;
 
     // Getters e Setters
 
+    // Getters e Setters
     public Long getSupplierId() {
         return supplier_id;
     }
@@ -28,59 +34,59 @@ public class Supplier {
         this.supplier_id = supplier_id;
     }
 
-    public String getSupplierName() {
+    public String getSupplier_name() {
         return supplier_name;
     }
 
-    public void setSupplierName(String supplier_name) {
+    public void setSupplier_name(String supplier_name) {
         this.supplier_name = supplier_name;
     }
 
-    public String getSupplierCountry() {
+    public String getSupplier_country() {
         return supplier_country;
     }
 
-    public void setSupplierCountry(String supplier_country) {
+    public void setSupplier_country(String supplier_country) {
         this.supplier_country = supplier_country;
     }
 
-    public String getSupplierStreet() {
+    public String getSupplier_street() {
         return supplier_street;
     }
 
-    public void setSupplierStreet(String supplier_street) {
+    public void setSupplier_street(String supplier_street) {
         this.supplier_street = supplier_street;
     }
 
-    public String getSupplierComplement() {
+    public String getSupplier_complement() {
         return supplier_complement;
     }
 
-    public void setSupplierComplement(String supplier_complement) {
+    public void setSupplier_complement(String supplier_complement) {
         this.supplier_complement = supplier_complement;
     }
 
-    public int getSupplierNumber() {
+    public Long getSupplier_number() {
         return supplier_number;
     }
 
-    public void setSupplierNumber(int supplier_number) {
+    public void setSupplier_number(long supplier_number) {
         this.supplier_number = supplier_number;
     }
 
-    public int getSupplierType() {
+    public int getSupplier_type() {
         return supplier_type;
     }
 
-    public void setSupplierType(int supplier_type) {
+    public void setSupplier_type(int supplier_type) {
         this.supplier_type = supplier_type;
     }
 
-    public String getSupplierDocument() {
+    public String getSupplier_document() {
         return supplier_document;
     }
 
-    public void setSupplierDocument(String supplier_document) {
+    public void setSupplier_document(String supplier_document) {
         this.supplier_document = supplier_document;
     }
 }
